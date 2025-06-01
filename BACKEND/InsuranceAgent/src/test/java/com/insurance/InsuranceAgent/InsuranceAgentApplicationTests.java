61732c44e8e27310994a7e5b004d8d9d752841f3
sumeetkadam <sumeetkadam22@gmail.com>
2025-06-01 18:33:57 +0530

    back001_spring application created
    springboot (maven,JAVA-17,3.5.0) application created with below details:
    1.Group- com.insurance
    2.Artifact- InsuranceAgent
    3.Packaging- Jar
    
    Dependencies:
    1.Spring Web
    2.Spring Data JPA
    3.Spring Security
    4.Lombok
    5.H2 Database
    6.MySQL Driver

---
+++ BACKEND/InsuranceAgent/src/test/java/com/insurance/InsuranceAgent/InsuranceAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.InsuranceAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
